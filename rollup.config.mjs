7cb35314092c5325dac2451ffe4d3bc7aacc097d
dimas.OA <dimas.oktaarifani@ccn.com.sg>
2023-06-17 04:18:23 +0700

    add react and react-dom as external package on rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -31,6 +31,7 @@ export default [
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()]
+    plugins: [dts()],
+    external: ["react", "react-dom"]
   },
 ];
