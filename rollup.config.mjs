85bb4a713300ca4290ab98fe7d3935ab51afbb0d
dimas.OA <dimas.oktaarifani@ccn.com.sg>
2023-06-17 04:47:22 +0700

    edit config rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,13 +1,61 @@
+import { readdirSync } from 'fs';
+import path from 'path';
+import babel from '@rollup/plugin-babel';
+import external from 'rollup-plugin-peer-deps-external';
 import resolve from "@rollup/plugin-node-resolve";
+import replace from '@rollup/plugin-replace';
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import terser from '@rollup/plugin-terser'
 import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 const packageJson = require('./package.json');
 
+
+const EXTENSIONS = ['.ts', '.tsx', '.js', '.jsx', '.json'];
+const CODES = [
+  'THIS_IS_UNDEFINED',
+  'MISSING_GLOBAL_NAME',
+  'CIRCULAR_DEPENDENCY',
+];
+
+const getChunks = URI =>
+  readdirSync(path.resolve(URI))
+    .filter(x => x.includes('.ts'))
+    .reduce((a, c) => ({ ...a, [c.replace('.ts', '')]: `src/${c}` }), {});
+
+const discardWarning = warning => {
+  if (CODES.includes(warning.code)) {
+    return;
+  }
+
+  console.error(warning);
+};
+
+const commonPlugins = () => [
+  external({
+        includeDependencies: true,
+      }),
+      babel({
+        babelrc: false,
+        presets: [['@babel/preset-env', { modules: false }], '@babel/preset-react'],
+        extensions: EXTENSIONS,
+        exclude: 'node_modules/**',
+      }),
+      commonjs({
+        include: /node_modules/,
+      }),
+      // replace({ 'process.env.NODE_ENV': JSON.stringify(env) }),
+      resolve({
+        extensions: EXTENSIONS,
+        preferBuiltins: false,
+      }),
+    ];
+
 export default [
   {
+    onwarn: discardWarning,
     input: "src/index.ts",
     output: [
       {
@@ -19,19 +67,27 @@ export default [
         file: packageJson.module,
         format: "esm",
         sourcemap: true,
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
       },
     ],
     plugins: [
-      resolve(),
-      commonjs(),
+      ...commonPlugins(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      [dts()]
+      dts(),
+      // terser()
     ],
   },
   {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-    external: ["react", "react-dom"]
+    onwarn: discardWarning,
+    input: getChunks('src'),
+    output: [
+      { dir: 'dist/esm', format: 'esm', sourcemap: true },
+      { dir: 'dist/cjs', format: 'cjs', exports: 'named', sourcemap: true },
+    ],
+    plugins: [...commonPlugins(), dts()],
+    // external: ["react", "react-dom"]
   },
 ];
--- src/hooks/useCounter/useCounter.tsx
+++ src/hooks/useCounter/useCounter.tsx
@@ -1,9 +1,7 @@
 import { useState } from 'react';
 
-export type count = number;
-
 function useCounter(){
-  const [count, setCount] = useState<count>(0)
+  const [count, setCount] = useState<number>(0)
   const increment = () => setCount((c) => c + 1)
   return { count, increment }
 }
