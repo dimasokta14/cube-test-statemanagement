661c53dcb0ab4c4bf2ecb8d993a78f264be9c438
dimas.OA <dimas.oktaarifani@ccn.com.sg>
2023-06-17 03:34:37 +0700

    init statemgmt

---
+++ src/hooks/useCounter/useCounter.test.tsx
@@ -0,0 +1,10 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useCounter } from './useCounter';
+
+it('should increment counter', () => {
+  const { result } = renderHook(() => useCounter())
+  act(() => {
+    result.current.increment()
+  })
+  expect(result.current.count).toBe(1)
+})
